77dc895565cd6cbf698ba43ec01303079a7402aa
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-08-01 19:41:20 -0700

    hooked up to mlab db and added car to db

--- models/index.js
+++ models/index.js
@@ -8,7 +8,9 @@ let config = require(__dirname + '/../config/env.js');
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
 
-mongoose.connect('mongodb://localhost/db', options);
+// mongoose.connect('mongodb://localhost/db', options);
+mongoose.connect(process.env.MONGOLAB_URI , options);
+
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
