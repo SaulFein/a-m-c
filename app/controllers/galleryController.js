67a5933696176d78088645117fa8b8a3058ef2a7
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-08-16 08:51:17 -0700

    styled nav bar

--- app/controllers/galleryController.js
+++ app/controllers/galleryController.js
@@ -9,11 +9,14 @@ galleryCtrl.controller('galleryController', function($scope, $http, $window, Car
           // console.log(JSON.stringify(data));
           $scope.cars = data;
           $window.localStorage.cars = JSON.stringify($scope.cars);
+              $('.carousel').bcSwipe({ threshold: 50 });
       })
       .error(function(data) {
           console.log('Error: ' + data);
       });
     }
+
+
     // $http.get('/car')
     //     .success(function(data){
     //         console.log(JSON.stringify(data));
