0ada1910e8c4a66c7ae736e59b57a87faf95f0c8
Brandon Feinstein <brandon@Brandons-MacBook-Pro.local>
2017-01-13 14:05:54 -0800

    including rli

---
+++ app/directives/routeLoadingIndicator.js
@@ -0,0 +1,18 @@
+var routeLoadingIndicator = angular.module('routeLoadingIndicator', [])
+routeLoadingIndicator.directive('routeLoadingIndicator', ["$rootScope", function($rootScope){
+  return {
+    restrict:'E',
+    template:"<h1 ng-if='isRouteLoading'>Loading...</h1>",
+    link:function(scope, elem, attrs){
+      scope.isRouteLoading = false;
+
+      $rootScope.$on('$routeChangeStart', function(){
+        scope.isRouteLoading = true;
+      });
+
+      $rootScope.$on('$routeChangeSuccess', function(){
+        scope.isRouteLoading = false;
+      });
+    }
+  };
+}]);
